efc980d3aeb7e06519cb6e2157f1646f148f9ca1
majedul <majedict34@gmail.com>
2021-07-08 05:02:43 +0600

    add login activity

--- app/src/main/java/com/itmedicious/spliff/activity/PrimaryActivity.kt
+++ app/src/main/java/com/itmedicious/spliff/activity/PrimaryActivity.kt
@@ -1,12 +1,26 @@
 package com.itmedicious.spliff.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
 import com.itmedicious.spliff.R
 
 class PrimaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_primary)
+        val join = findViewById<MaterialButton>(R.id.join)
+        val login = findViewById<MaterialButton>(R.id.log_in)
+
+        join.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
