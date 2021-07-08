93e0f39d700e503e6d6eedede5ff731ed955ea0c
majedul <majedict34@gmail.com>
2021-07-08 18:29:24 +0600

    add drawer layout

--- app/src/main/java/com/itmedicious/spliff/activity/LoginActivity.kt
+++ app/src/main/java/com/itmedicious/spliff/activity/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.itmedicious.spliff.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.itmedicious.spliff.R
+import androidx.appcompat.app.AppCompatActivity
+import com.itmedicious.spliff.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding?.createAccountButton?.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
     }
 }
